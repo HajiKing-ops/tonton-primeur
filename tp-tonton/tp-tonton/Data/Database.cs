fa384608f2597a9407e14c71ec23fe7b82880ab0
Roman <romansalamzada50@gmail.com>
2026-09-06 18:08:03 +0200

    connect MySql and load article into DataGridView

---
+++ tp-tonton/tp-tonton/Data/Database.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Text;
+using MySqlConnector;
+
+namespace tp_tonton.Data
+{
+    internal class Database
+    {
+        string connectionString = "Server=localhost;Port=3306;Database=stock_fruits_legumes;User ID=root;Password=;";
+        public MySqlConnection GetConnection()
+        {
+            
+            return new MySqlConnection(connectionString);
+
+
+        }
+        
+       
+    }
+}
--- tp-tonton/tp-tonton/Form1.cs
+++ tp-tonton/tp-tonton/Form1.cs
@@ -1,3 +1,7 @@
+using tp_tonton.Data;
+using MySqlConnector;
+using tp_tonton.Data;
+using System.Data;
 namespace tp_tonton
 {
     public partial class Form1 : Form
@@ -5,6 +9,7 @@ namespace tp_tonton
         public Form1()
         {
             InitializeComponent();
+            ChargerArticle();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,5 +36,60 @@ namespace tp_tonton
         {
 
         }
+
+        private void TesterConnexion()
+        {
+            try
+            {
+                Database database = new Database();
+
+                using (var connexion = database.GetConnection())
+                {
+                    connexion.Open();
+                    MessageBox.Show("Connexion ressie");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed" + ex.Message);
+            }
+    
+        }
+
+        private void ChargerArticle()
+        {
+            try
+            {
+                Database database = new Database();
+                using (var connexion = database.GetConnection())
+                {
+                    string query = @"Select
+                                a.id_article,
+                                a.nom as nom_article,
+                                a.type,
+                                a.prix_unitaire,
+                                a.quantite_stock,
+                                f.nom as nom_fournisseur
+                    from article as a inner join fournisseur as f  on a.id_fournisseur = f.id_fournisseur;";
+                    using (var command = new MySqlCommand(query, connexion))
+                    {
+                        using (var adapter = new MySqlDataAdapter(command))
+                        {
+                            DataTable table = new DataTable();
+                            connexion.Open();
+                            adapter.Fill(table);
+
+                            dgvArticles.DataSource = table;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des articles : " + ex.Message);
+            }
+        }
+
     }
 }
